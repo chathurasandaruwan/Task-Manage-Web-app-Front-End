73543de886acb33af442d9ef6a696f9098ddf409
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-04-07 20:08:48 +0530

    modify task list page component

--- src/app/pages/task-list/task-list.component.ts
+++ src/app/pages/task-list/task-list.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task, TaskStatus } from '../../models/task.model';
+import { TaskService } from '../../services/task.service';
+import { NgFor, NgIf } from '@angular/common';
+import { TaskCardComponent } from '../../components/task-card/task-card.component';
+import { TaskFilterComponent } from '../../components/task-filter/task-filter.component';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [NgIf, TaskCardComponent, NgFor, TaskFilterComponent],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = []
+  loading = true
 
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.loadTasks()
+  }
+
+  loadTasks(title = "", status: TaskStatus | "" = ""): void {
+    this.loading = true
+    this.taskService.filterTasks(title, status).subscribe((tasks) => {
+      this.tasks = tasks
+      this.loading = false
+    })
+  }
+
+  onFilterChange(filters: { title: string; status: TaskStatus | "" }): void {
+    this.loadTasks(filters.title, filters.status)
+  }
+
+  onDeleteTask(id: number): void {
+    if (confirm("Are you sure you want to delete this task?")) {
+      console.log('need delete id ', id);
+      
+    }
+  }
 }
