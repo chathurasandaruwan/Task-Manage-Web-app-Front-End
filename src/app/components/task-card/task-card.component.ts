fe7616ac54fd9df03a2510aab02f08bc2a744a49
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-04-07 19:24:25 +0530

    update task card

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from '../../models/task.model';
+import { RouterLink } from '@angular/router';
+import { DatePipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-task-card',
-  imports: [],
+  imports: [RouterLink, NgClass, DatePipe],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.scss'
 })
 export class TaskCardComponent {
+  @Input() task!: Task
+  @Output() delete = new EventEmitter<number>()
 
+  onDelete(): void {
+    this.delete.emit(this.task.id)
+  }
+
+  getStatusClass(): string {
+    switch (this.task.status) {
+      case "TO_DO":
+        return "bg-warning"
+      case "IN_PROGRESS":
+        return "bg-info"
+      case "DONE":
+        return "bg-success"
+      default:
+        return "bg-secondary"
+    }
+  }
 }
