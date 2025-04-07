4fdfec0fda1e7d4e3ea5b22ecd880574bc3bc241
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-04-07 14:17:58 +0530

    create task model

---
+++ src/app/models/task.model.ts
@@ -0,0 +1,10 @@
+export type TaskStatus = "TO_DO" | "IN_PROGRESS" | "DONE"
+
+export interface Task {
+  id: number
+  title: string
+  description: string
+  status: TaskStatus
+  createdAt: string
+  updatedAt: string
+}
