21b9503de83f82cad8750769eda17d34303340ad
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-04-07 14:46:47 +0530

    set navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full'
+      },
+      {
+        path: 'tasks',
+        loadComponent: () => import('./pages/task-list/task-list.component').then(m => m.TaskListComponent)
+      },
+      {
+        path: 'tasks/new',
+        loadComponent: () => import('./pages/task-form/task-form.component').then(m => m.TaskFormComponent)
+      },
+      {
+        path: 'tasks/:id',
+        loadComponent: () => import('./pages/task-detail/task-detail.component').then(m => m.TaskDetailComponent)
+      },
+      {
+        path: 'tasks/:id/edit',
+        loadComponent: () => import('./pages/task-form/task-form.component').then(m => m.TaskFormComponent)
+      },
+      {
+        path: '**',
+        redirectTo: 'tasks'
+      }
+];
