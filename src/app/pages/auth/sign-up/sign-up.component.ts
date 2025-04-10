dca4dc858e027e56f116d1c23bce11e96791a093
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-04-10 16:09:10 +0530

    create sign up page

--- src/app/pages/auth/sign-up/sign-up.component.ts
+++ src/app/pages/auth/sign-up/sign-up.component.ts
@@ -1,11 +1,82 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule ,RouterLink],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+  signUpForm: FormGroup
+  loading = false
+  submitted = false
+  error = ""
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+  ) {
+    this.signUpForm = this.fb.group(
+      {
+        email: ["", [Validators.required, Validators.email]],
+        password: ["", [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ["", Validators.required],
+      },
+      {
+        validator: this.mustMatch("password", "confirmPassword"),
+      },
+    )
+  }
+
+  get f() {
+    return this.signUpForm.controls
+  }
+
+  mustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName]
+      const matchingControl = formGroup.controls[matchingControlName]
+
+      if (matchingControl.errors && !matchingControl.errors['mustMatch']) {
+        // return if another validator has already found an error
+        return
+      }
+
+      // set error on matchingControl if validation fails
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true })
+      } else {
+        matchingControl.setErrors(null)
+      }
+    }
+  }
+
+  onSubmit(): void {
+    this.submitted = true
+    this.error = ""
+
+    // Stop if form is invalid
+    if (this.signUpForm.invalid) {
+      return
+    }
+
+    this.loading = true
+    this.authService
+      .register(this.signUpForm.value.email, this.signUpForm.value.password)
+      .subscribe({
+        next: () => {
+          this.router.navigate(["/tasks"])
+        },
+        error: (error) => {
+          this.error = error.message
+          this.loading = false
+        },
+      })
+  }
 
 }
