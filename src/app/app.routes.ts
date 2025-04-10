09ffff62237edd1b99873574de0a936a5914f5fb
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-04-10 12:54:12 +0530

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,37 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'full'
-      },
-      {
-        path: 'tasks',
-        loadComponent: () => import('./pages/task-list/task-list.component').then(m => m.TaskListComponent)
-      },
-      {
-        path: 'tasks/new',
-        loadComponent: () => import('./pages/task-form/task-form.component').then(m => m.TaskFormComponent)
-      },
-      {
-        path: 'tasks/:id',
-        loadComponent: () => import('./pages/task-detail/task-detail.component').then(m => m.TaskDetailComponent)
-      },
-      {
-        path: 'tasks/:id/edit',
-        loadComponent: () => import('./pages/task-form/task-form.component').then(m => m.TaskFormComponent)
-      },
-      {
-        path: '**',
-        redirectTo: 'tasks'
-      }
+  {
+    path: "",
+    redirectTo: "signin",
+    pathMatch: "full",
+  },
+  {
+    path: "signin",
+    loadComponent: () => import("./pages/auth/sign-in/sign-in.component").then((m) => m.SignInComponent),
+  },
+  {
+    path: "signup",
+    loadComponent: () => import("./pages/auth/sign-up/sign-up.component").then((m) => m.SignUpComponent),
+  },
+  {
+    path: "tasks",
+    loadComponent: () => import("./pages/task-list/task-list.component").then((m) => m.TaskListComponent),
+  },
+  {
+    path: "tasks/new",
+    loadComponent: () => import("./pages/task-form/task-form.component").then((m) => m.TaskFormComponent),
+  },
+  {
+    path: "tasks/:id",
+    loadComponent: () => import("./pages/task-detail/task-detail.component").then((m) => m.TaskDetailComponent),
+  },
+  {
+    path: "tasks/:id/edit",
+    loadComponent: () => import("./pages/task-form/task-form.component").then((m) => m.TaskFormComponent),
+  },
+  {
+    path: "**",
+    redirectTo: "signin",
+  },
 ];
