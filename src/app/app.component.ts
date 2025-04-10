c626d1daccf5d4f993db1a046fb3fa790bb1a1e6
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-04-10 14:06:19 +0530

    hide navbar from auth pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,22 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'task-manager-main';
+  isAuthPage = false
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Check the initial route
+    this.checkIfAuthPage(this.router.url)
+
+    // Subscribe to route changes
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event: any) => {
+      this.checkIfAuthPage(event.url)
+    })
+  }
+
+  private checkIfAuthPage(url: string): void {
+    // Hide navbar on auth pages
+    this.isAuthPage = url.includes("/signin") || url.includes("/signup")
+  }
 }
